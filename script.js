a5ffd6c7e614833a6ffc4e9aea1fe9d2ad914d30
Android-Nougat <42477290+Adhishreya@users.noreply.github.com>
2021-09-07 20:31:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var billValue = 0,peopleCount=0,rateValue=0,tipAmount = 0.0,totalAmount = 0.0 ;
 const tipAmountElement =  document.getElementsByClassName("tip-amount")[0];
 const totalAmountElement =  document.getElementsByClassName("total-amount")[0];
+const peopleCountElement = document.getElementsByClassName("people-input")[0];
 const resetButtton = document.getElementsByClassName("reset")[0];
+const billCountElement = document.getElementsByClassName("bill-input")[0];
 resetButtton.disabled = true;
 
 resetButtton.addEventListener("click",()=>{
-    console.log("click")
+    // console.log("click")
     totalAmountElement.innerHTML = `$0.00`;
     tipAmountElement.innerHTML = `$0.00`;
+    billCountElement.value = '';
+    peopleCountElement.value = '';
 });
 function setValue()
 {
@@ -16,42 +20,34 @@ function setValue()
         tipAmount = (billValue*rateValue)/(peopleCount*100);
         totalAmount = ((billValue*rateValue*0.01)+billValue)/peopleCount;
         resetButtton.disabled = false;
-        // /Number(peopleCount);
     }
-    console.log(totalAmount);
-    // console.log(document.getElementsByClassName("tip-amount")[0])
-    
-    // document.getElementsByClassName("tip-amount")[0].innerHTML =`<h1>$${totalAmount.toFixed(2).toString()}</h1>`;
-    
-    
+    // console.log(totalAmount);
     totalAmountElement.innerHTML = `$${totalAmount.toFixed(2).toString()}`;
     tipAmountElement.innerHTML = `$${tipAmount.toFixed(2).toString()}`;
-    // console.log(totalAmount)
 }
 Array.from(document.getElementsByClassName("rate-button")).forEach((element,i)=>{
     element.addEventListener("click",()=>{
         rateValue=Number(element.value);
         setValue();
-        console.log(rateValue);
+        // console.log(rateValue);
     });
 
-    // console.log(element)
 });
 document.getElementsByClassName("editable")[0].addEventListener("input",(e)=>{
     rateValue = Number(e.target.value);
     setValue();
-    console.log(rateValue);
+    // console.log(rateValue);
 });
 
-document.getElementsByClassName("bill-input")[0].addEventListener("input",(e)=>{
+billCountElement.addEventListener("input",(e)=>{
     billValue = Number(e.target.value);
     setValue();
-    console.log(billValue);
+    // console.log(billValue);
 });
 
-document.getElementsByClassName("people-input")[0].addEventListener("input",(e)=>{
+peopleCountElement.addEventListener("input",(e)=>{
     peopleCount = Number(e.target.value);
-    console.log(typeof Number(peopleCount));
+    // console.log(typeof Number(peopleCount));
     
     if(peopleCount == 0)
     {   
@@ -68,5 +64,4 @@ document.getElementsByClassName("people-input")[0].addEventListener("input",(e)=
         setValue();
 
     }
-})
-// console.log(value);
+});
